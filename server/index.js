37a6f38a60a6b9fc566f867859151a4b1761afc5
Seth Jensen <the.anonymous.mustache@gmail.com>
2022-01-30 14:50:34 -0700

    metmeuseum api project submission
    made a simple ui to query and display results from the metropolitan museum of art collection api, enjoy!

--- server/index.js
+++ server/index.js
@@ -1,11 +1,132 @@
-const express = require('express')
+const express = require('express');
+const https = require('https');
+const path = require('path');
+const _ = require('lodash');
 const app = express()
+
 const port = 3011;
 
-app.get('/api/', (req, res) => {
-  res.send({"success": true, data: "Hello World!"})
-})
+const baseUrl = "https://collectionapi.metmuseum.org/public/collection/v1/";//base url for the met museum api
+const objectDisplayLimit = 7;//sets the limit of how many objects data we fetch/display from the search results. number can be very big
 
+//list of paired down parameters from artpeice object data
+let objectDataSubset = ({objectID, primaryImageSmall, title, artistDisplayName, medium, department}) => ({objectID, primaryImageSmall, title, artistDisplayName, medium, department});
+
+app.get("/index.js", (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.js'));
+});
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+ 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+//department data endpoint, gets the list of departments and their ids
+app.get('/api-departments/', (req, res) => {
+  let http_promise = apiCall("departments")
+  http_promise.then(results => 
+    {
+      res.send({"success": true, data: results})
+    });
+})
+
+//search query endpoint
+app.get('/api-query/', (req, res) => {
+  let queryParams = constructQueryParameters(req.query)
+  let http_promise = apiCall("search", queryParams)
+  http_promise.then(results => 
+    {
+ 
+      if(results.total > 0){
+        results = results.objectIDs.slice(0, objectDisplayLimit)//get subset of results since we can get a ton
+        //loop through returned object ids and fetch the first fews data so we can display the results
+        let resultsProcessedPromise = processQueryResults(results)
+        resultsProcessedPromise.then(resultsProcessed => {
+          res.send({"success": true, data: resultsProcessed});
+        })
+      } else {//we did not get any results
+        res.send({"success": true, data:false});
+      }
+      
+    });
+})
+
+//loops through the object ids returned and fetches the objects data
+const processQueryResults = async function(results){
+  return await Promise.all(_.map(results, async function(objectId){
+    let http_objectPromise = getObjectData(objectId)
+    return http_objectPromise.then(objectData => 
+      {
+        let objectReduced = objectDataSubset(objectData)
+        return objectReduced;
+      })
+  }))
+}
+
+//fetches individual object/art peices data from the api
+const getObjectData = function (objectId){
+  return new Promise((resolve) => {
+    let http_promise = apiCall(`objects/${objectId}`)
+    http_promise.then(results => 
+      {
+        //console.log(results)
+        resolve(results);
+      });
+    });
+}
+
+//builds the parameters for the query url
+//the Met Meseum of Art requires the q parameter for all search requests
+//the q parameter seems to just match the text with anything in the metadata for the object
+const constructQueryParameters = function(request){
+  let queryParams = `?q=${encodeURIComponent(request.text)}`;
+
+  if(request.dept != "*"){
+    queryParams += `&departmentId=${encodeURIComponent(request.dept)}`
+  }
+  if(request.artist != ""){
+      queryParams += `&artistOrCulture=${encodeURIComponent(request.artist)}`
+  }
+  if(request.medium != ""){
+      queryParams += `&medium=${encodeURIComponent(request.medium)}`
+  }
+  return queryParams;
+}
+
+
+const apiCall = function (endPoint, parameters = "") {
+  //console.log("requestURL: ", baseUrl + endPoint + parameters)
+  let data = '';
+  return new Promise((resolve, reject) => {
+    https.get(baseUrl + endPoint + parameters, (resp) => {
+      
+      const { statusCode } = resp;
+      const contentType = resp.headers['content-type'];
+
+      if (statusCode !== 200) {
+        error = new Error('Request Failed.\n' +
+                          `Status Code: ${statusCode}`);
+      } else if (!/^application\/json/.test(contentType)) {
+        error = new Error('Invalid content-type.\n' +
+                          `Expected application/json but received ${contentType}`);
+      }
+      
+      // A chunk of data has been received.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. 
+      resp.on('end', () => {
+        const parsedData = JSON.parse(data);
+        resolve(parsedData);
+      });
+
+    }).on("error", (err) => {
+      reject("Error: " + err.message);
+    })
+  })
+}
+
--- src/App.js
+++ src/App.js
@@ -1,28 +1,167 @@
 import React, {useEffect, useState} from 'react';
+import _, { result } from 'lodash';
 
 function App() {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(true);
+  const [queryResultData, setQueryResultData] = useState();
+  const [departments, setDepartments] = useState({});
+  const [departmentFilter, setDepartmentFilter] = useState("*");
+  const [artistFilter, setArtistFilter] = useState("");
+  const [mediumFilter, setMediumFilter] = useState("");
+  const [textFilter, setTextFilter] = useState("");
+  const [loadingFilters, setLoadingFilters] = useState(true);//loading status for the filters, right now its just dept data
+  const [loadingQuery, setLoadingQuery] = useState(true);//loading status for the main search query
+  const [loadingQueryText, setLoadingQueryText] = useState("...");
 
-  useEffect(  () => {
-    async function fetchData() {
-      setLoading(true);
-      const response = await fetch('/api');
-      const results = await response.json();
-      if(results.success) {
-          setData(results.data);
-      }
-      setLoading(false);
-    }
-    fetchData();
+  useEffect(() => {
+    fetchDepartmentData();
   }, [])
+
+  const fetchDepartmentData = async() => {
+    let fetchUrl = `/api-departments`
+    setLoadingFilters(true);
+    const response = await fetch(fetchUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    const results = await response.json();
+    if(results.success) {
+      //console.log("department data: ", results.data);
+      setDepartments(results.data.departments);
+    }
+    setLoadingFilters(false);
+  }
+
+  const fetchQuery = async() => {
+    let fetchUrl = `/api-query?dept=${departmentFilter}&artist=${artistFilter}&medium=${mediumFilter}&text=${textFilter}`;//pass in all our filters
+    setLoadingQueryText("Loading ...");
+    setLoadingQuery(true);
+
+    const response = await fetch(fetchUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    const results = await response.json();
+    if(results.success) {
+      console.log("Query results data: ", results.data);
+      if(results.data != false){
+        
+        setQueryResultData(results.data);
+        setLoadingQuery(false);
+      } else {
+        setQueryResultData({});
+        setLoadingQueryText("No Results Found")
+        setLoadingQuery(true);
+      }
+    }
+  }
+
+  const preSubmitValidation = () => {
+    if(textFilter.length < 1){
+      alert("You must enter something in the search bar first!");
+      return false;
+    }
+    fetchQuery();
+  }
+
+  const departmentFilterUpdate = (event) => {
+    setDepartmentFilter(event.target.value);
+  }
+
+  const artistFilterUpdate = (event) => {
+    setArtistFilter(event.target.value);
+  }
+
+  const mediumFilterUpdate = (event) => {
+    setMediumFilter(event.target.value);
+  }
+
+  const textFilterUpdate = (event) => {
+    setTextFilter(event.target.value);
+  }
+
+  
 
   return (
     <div className="App">
-      {loading ? <div>Loading...</div> : data}
-      {/*Insert UI elements and components here*/}
+      {loadingFilters ? <div>Loading Filter Data...</div> :
+      <div className="formFilter">
+        <div>
+        <div>
+          <label>
+            Search Term:
+            <input name="Text Filter" type="text" value={textFilter} onChange={textFilterUpdate}></input>
+          </label>
+        </div>
+          <label>
+            Department Filter: 
+          <select value={departmentFilter} onChange={departmentFilterUpdate}>
+            <option value="*">All Departments</option>
+            {_.map(departments, function(department) {
+              return (<option value={department.departmentId}>{department.displayName}</option>)
+              })
+            }
+          </select>
+          </label>
+        </div>
+        <div>
+          <label>
+            Artist Filter: 
+            <input name="Artist Filter" type="text" value={artistFilter} onChange={artistFilterUpdate}></input>
+          </label>
+        </div>
+        <div>
+          <label>
+            Medium Filter:
+            <input name="Medium Filter" type="text" value={mediumFilter} onChange={mediumFilterUpdate}></input>
+          </label>
+        </div>
+        <div>
+          <button onClick={preSubmitValidation}> Search!</button>
+        </div>
+        <div>
+          {loadingQuery ? <div>{loadingQueryText}</div> :<div>
+            <table>
+              <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Medium</th>
+                <th>Author</th>
+                <th>department</th>
+              </tr>
+              {_.map(queryResultData, function(artpeice){
+                return(
+                  <tr>
+                    <td>
+                      <img src={artpeice.primaryImageSmall} alt={artpeice.title}/>
+                    </td>
+                    <td>
+                      <b>{artpeice.title}</b>
+                    </td>
+                    <td>
+                      <b>{artpeice.medium}</b>
+                    </td>
+                    <td>
+                      by: {artpeice.artistDisplayName == "" ? "Unknown": artpeice.artistDisplayName}
+                    </td>
+                    <td>
+                      <b>{artpeice.department}</b>
+                    </td>
+                  </tr>
+                )
+              })}
+            </table>
+          </div>}
+        </div>
+      </div>
+      }
     </div>
   );
 }
+
 
 export default App;
